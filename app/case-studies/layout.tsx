54d28dd2e86a6a5158c5f5537c400aad50d87255
lbsudo <lbsudo100@gmail.com>
2023-09-21 00:29:49 -0700

    Site map updated and robots.ts file added

---
+++ app/case-studies/layout.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+import { siteConfig } from "@/config/site";
+
+export const metadata: Metadata = {
+  title: {
+    default: `Case Studies - ${siteConfig.name}`,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: `Get in touch with us. We'd love to talk more about how we can assist your business in increasing traffic, conversion, and growth. Reach out today and schedule a free discovery call to find out how we can help you solve the pain points in your business.`,
+};
+
+
+export default function ContactLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="flex flex-col w-full items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-full text-center justify-center">
+        {children}
+      </div>
+    </section>
+  );
+}
---
+++ app/case-studies/page.tsx
@@ -0,0 +1,43 @@
+"use client"
+import React from 'react'
+import { motion, useAnimation } from 'framer-motion'
+import Image from 'next/image';
+
+export default function IndustriesPage() {
+
+  const controls = useAnimation();
+
+  React.useEffect(() => {
+    // Animate the div in the first section when the page loads
+    controls.start({ opacity: 1, y: 0, transition: { duration: 1 } });
+    // Fetch post metadata from the API route
+  }, [controls]);
+
+  return (
+    <div className="mb-16">
+      <section className='py-40 flex justify-center flex-col w-full relative z-0' >
+        <div className="absolute inset-0 w-full h-full">
+          <Image
+            src="/industry.jpg"
+            alt="Background Image"
+            layout="fill"
+            objectFit="cover"
+            priority
+          />
+        </div>
+        <div className="absolute inset-0 w-full h-full bg-gradient-to-b from-black via-black to-black opacity-50"></div>
+        <motion.div className='relative w-full text-white flex flex-col justify-center items-center '
+          initial={{ opacity: 0, y: 50 }}
+          animate={controls}
+        >
+          <h1 className='text-5xl lg:text-6xl w-3/4 h-1/2 text-center mt-8'>Industries We Work With</h1>
+          <h4 className='text-4xl w-3/4 h-1/4 mt-16 mb-16 text-center'>We work with a wide ranges of businesses that face similar problems but need specialized solutions that fit their needs these are just some of the industries we work with.</h4>
+        </motion.div>
+      </section>
+      <section>
+        {/* <IndustryGrid /> */}
+      </section>
+    </div>
+  );
+}
+
